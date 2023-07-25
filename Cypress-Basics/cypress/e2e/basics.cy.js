d8b83ac977ee23da297a974c1b47ab4d3b288559
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-25 11:36:41 -0700

    get() vs find()

--- Cypress-Basics/cypress/e2e/basics.cy.js
+++ Cypress-Basics/cypress/e2e/basics.cy.js
@@ -4,6 +4,7 @@ describe('tasks page', () => {
   it('should render the main image', () => {
     cy.visit('http://localhost:5173/');
     cy.get('.main-header img');
+    // cy.get('.main-header').find('img');
   });
 
   it('should display page title', () => {
