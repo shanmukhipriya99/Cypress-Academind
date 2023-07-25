783f3c09841daf2f53a9ba5eace4f9d2971db8e7
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-25 10:53:33 -0700

    selecting by text

--- Cypress-Basics/cypress/e2e/basics.cy.js
+++ Cypress-Basics/cypress/e2e/basics.cy.js
@@ -5,4 +5,9 @@ describe('tasks page', () => {
     cy.visit('http://localhost:5173/');
     cy.get('.main-header img');
   });
+
+  it('should display page title', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('h1').contains('My Cypress Course Tasks');
+  });
 });
--- Cypress-Basics/src/components/Header.jsx
+++ Cypress-Basics/src/components/Header.jsx
@@ -3,9 +3,9 @@ import logo from '../assets/logo.png';
 
 function Header() {
   return (
-    <header className="main-header">
-      <img src={logo} alt="A list" />
-      <h1>React Tasks</h1>
+    <header className='main-header'>
+      <img src={logo} alt='A list' />
+      <h1>My Cypress Course Tasks</h1>
     </header>
   );
 }
