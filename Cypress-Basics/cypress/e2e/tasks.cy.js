ac79d71104c762ed6802152fad28abe844e10a7b
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-26 09:56:41 -0700

    selecting dropdown values

--- Cypress-Basics/cypress/e2e/tasks.cy.js
+++ Cypress-Basics/cypress/e2e/tasks.cy.js
@@ -16,7 +16,7 @@ describe('tasks management', () => {
     cy.contains('Add Task').click();
     cy.get('#title').type('Sample Task');
     cy.get('#summary').type('Sample Summary');
-    // cy.get('#category').select('Moderate');
+    // cy.get('#category').select('moderate');
     cy.get('.modal').contains('Add Task').click();
     cy.get('.backdrop').should('not.exist');
     cy.get('.modal').should('not.exist');
@@ -29,5 +29,20 @@ describe('tasks management', () => {
     cy.get('.modal').contains('Add Task').click();
     // cy.get('.error-message').should('exist');
     cy.contains('Please provide values');
+  });
+  it('should filter tasks', () => {
+    cy.visit('http://localhost:5173/');
+    cy.contains('Add Task').click();
+    cy.get('#title').type('New Task');
+    cy.get('#summary').type('New Summary');
+    cy.get('#category').select('urgent');
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task').should('have.length', 1);
+    cy.get('#filter').select('moderate');
+    cy.get('.task').should('have.length', 0);
+    cy.get('#filter').select('urgent');
+    cy.get('.task').should('have.length', 1);
+    cy.get('#filter').select('all');
+    cy.get('.task').should('have.length', 1);
   });
 });
