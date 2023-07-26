29753ae890c610b6b99148c7a14a4d4d4d41ec71
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-25 20:45:52 -0700

    testing modals

--- Cypress-Basics/cypress/e2e/tasks.cy.js
+++ Cypress-Basics/cypress/e2e/tasks.cy.js
@@ -1,9 +1,12 @@
 /// <reference types='Cypress' />
 
 describe('tasks management', () => {
-    it('should open and close the new task model', () => {
-        cy.visit('http://localhost:5173/');
-        cy.contains('Add Task').click();
-        cy.get('.backdrop').click({force: true});
-    })
+  it('should open and close the new task model', () => {
+    cy.visit('http://localhost:5173/');
+    cy.contains('Add Task').click();
+    // cy.get('.backdrop').click({ force: true });
+    // cy.get('.backdrop').should('not.exist');
+    cy.contains('Cancel').click();
+    cy.get('.modal').should('not.exist');
+  });
 });
