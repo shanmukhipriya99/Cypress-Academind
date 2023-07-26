ecf8b7c385df553d726c76a2f27bee04a99b01d2
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-25 21:46:22 -0700

    simulating keyboard typing

--- Cypress-Basics/cypress/e2e/tasks.cy.js
+++ Cypress-Basics/cypress/e2e/tasks.cy.js
@@ -11,4 +11,14 @@ describe('tasks management', () => {
     cy.contains('Cancel').click();
     cy.get('.modal').should('not.exist');
   });
+  it('should create a new task', () => {
+    cy.visit('http://localhost:5173/');
+    cy.contains('Add Task').click();
+    cy.get('#title').type('Sample Task');
+    cy.get('#summary').type('Sample Summary');
+    // cy.get('#category').select('Moderate');
+    cy.get('.modal').contains('Add Task').click();
+    cy.get('.task h2').contains('Sample Task');
+    cy.get('.task p').contains('Sample Summary');
+  });
 });
