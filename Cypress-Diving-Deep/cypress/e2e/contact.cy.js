88d8bb31f0f0762f716cd51cf625919d92e21df6
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-26 18:00:50 -0700

    aliases

--- Cypress-Diving-Deep/cypress/e2e/contact.cy.js
+++ Cypress-Diving-Deep/cypress/e2e/contact.cy.js
@@ -6,8 +6,9 @@ describe('contact form', () => {
     cy.get('[data-cy="contact-input-message"]').type('Some random message');
     cy.get('[data-cy="contact-input-name"]').type('Random Message');
     cy.get('[data-cy="contact-input-email"]').type('random@message.com');
-    cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]')
+    cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
+    cy.get('@submitBtn').click();
+    cy.get('@submitBtn')
       .contains('Sending...')
       .should('have.attr', 'disabled');
   });
