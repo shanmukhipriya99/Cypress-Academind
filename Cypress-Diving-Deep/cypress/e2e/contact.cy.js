57059888ca7dcc035fce40c7ce82131ffe366930
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-26 20:50:36 -0700

    submitting empty form-1

--- Cypress-Diving-Deep/cypress/e2e/contact.cy.js
+++ Cypress-Diving-Deep/cypress/e2e/contact.cy.js
@@ -6,11 +6,21 @@ describe('contact form', () => {
     cy.get('[data-cy="contact-input-message"]').type('Some random message');
     cy.get('[data-cy="contact-input-name"]').type('Random Message');
     cy.get('[data-cy="contact-btn-submit"]').then((el) => {
-        expect(el.attr('disabled')).to.be.undefined;
+      expect(el.attr('disabled')).to.be.undefined;
     });
     cy.get('[data-cy="contact-input-email"]').type('random@message.com{enter}');
     cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
-    cy.get('@submitBtn').click();
+    // cy.get('@submitBtn').click();
     cy.get('@submitBtn').contains('Sending...').should('have.attr', 'disabled');
+  });
+  it('should validate the form input', () => {
+    cy.visit('http://localhost:5173/about');
+    // cy.get('[data-cy="contact-input-message"]').type('Some random message');
+    // cy.get('[data-cy="contact-input-name"]').type('Random Message');
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el).to.not.have.attr('disabled');
+      expect(el.text()).to.not.be.equal('Sending...');
+    });
   });
 });
