e716cbfc66503942d98f75a9ddb3927c8fc7cbc0
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-26 17:15:57 -0700

    testing page navigation

--- Cypress-Diving-Deep/cypress/e2e/tests.cy.js
+++ Cypress-Diving-Deep/cypress/e2e/tests.cy.js
@@ -1,9 +1,16 @@
 /// <reference types='Cypress' />
 
 describe('page navigation', () => {
-    it('should navigate between pages', () => {
-        cy.visit('http://localhost:5173/');
-        // cy.get('nav').contains('About').click();
-        cy.get('[data-cy="header-about-link"]').click();
-    });
-})
+  it('should navigate between pages', () => {
+    cy.visit('http://localhost:5173/');
+    // cy.get('nav').contains('About').click();
+    cy.get('[data-cy="header-about-link"]').click();
+    cy.location('pathname').should('eq', '/about');
+    cy.get('[data-cy="header-home-link"]').click();
+    cy.location('pathname').should('eq', '/');
+    
+    cy.get('[data-cy="header-about-link"]').click();
+    cy.go('back');
+    cy.location('pathname').should('eq', '/');
+  });
+});
