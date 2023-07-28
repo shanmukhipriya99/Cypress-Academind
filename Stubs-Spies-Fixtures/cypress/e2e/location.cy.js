1f4584823e7d0991d0769bf22fc389547463a30d
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-28 08:45:55 -0700

    beforeEach() hook

--- Stubs-Spies-Fixtures/cypress/e2e/location.cy.js
+++ Stubs-Spies-Fixtures/cypress/e2e/location.cy.js
@@ -1,24 +1,25 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
-  it('should fetch the user location', () => {
+  beforeEach(() => {
     cy.visit('/').then((windw) => {
       cy.stub(windw.navigator.geolocation, 'getCurrentPosition')
         .as('getUserLocation')
         .callsFake((cb) => {
           setTimeout(() => {
-            cb({
-              coords: {
-                latitude: 37.5,
-                longitude: 83.43,
-              },
-            });
+            cb({ coords: { latitude: 39.9, longitude: 48.03 } });
           }, 100);
         });
     });
+  });
+  it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserLocation').should('have.been.called');
     cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
     cy.get('[data-cy="actions"]').should('contain', 'Location fetched'); // alternative to contains()
+  });
+  it('should share a location URL', () => {
+    cy.get('[data-cy="name-input"]').type('John Doe');
+    cy.get('[data-cy="get-loc-btn"]').click();
   });
 });
