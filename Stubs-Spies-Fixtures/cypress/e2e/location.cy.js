58f9cff2ce6e5e6b486558639b54965d31d75d10
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-27 17:33:32 -0700

    creating and using stub

--- Stubs-Spies-Fixtures/cypress/e2e/location.cy.js
+++ Stubs-Spies-Fixtures/cypress/e2e/location.cy.js
@@ -2,7 +2,12 @@
 
 describe('share location', () => {
   it('should fetch the user location', () => {
-    cy.visit('/');
+    cy.visit('/').then((windw) => {
+      cy.stub(windw.navigator.geolocation, 'getCurrentPosition').as(
+        'getUserLocation'
+      );
+    });
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getUserLocation').should('have.been.called');
   });
 });
