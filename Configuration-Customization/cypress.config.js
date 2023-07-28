b378bd36b3f2d97870eb3b1c95a251456397e93a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-27 17:17:41 -0700

    cy.task()

--- Configuration-Customization/cypress.config.js
+++ Configuration-Customization/cypress.config.js
@@ -1,9 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('task', {
+        seedDatabase() {
+          // Run your Node.js code
+        }
+      });
     },
   },
 });
--- Configuration-Customization/cypress/e2e/contact.cy.js
+++ Configuration-Customization/cypress/e2e/contact.cy.js
@@ -3,6 +3,7 @@
 describe('contact form', () => {
   it('should submit the form', () => {
     cy.visit('http://localhost:5173/about');
+    cy.task('seedDatabase');
     cy.getById('contact-input-message').type('Hello world!');
     cy.getById('contact-input-name').type('John Doe');
     cy.getById('contact-btn-submit').then((el) => {
