512b1e8e55798ce0fbc9d8d07a35da943cb84316
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-28 15:22:30 -0700

    should display validation errors

--- Network-Requests/cypress/e2e/newsletter.cy.js
+++ Network-Requests/cypress/e2e/newsletter.cy.js
@@ -11,4 +11,13 @@ describe('Newsletter', () => {
     cy.get('[data-cy="newsletter-submit"]').click();
     cy.contains('Thanks for signing up');
   });
+  it('should display validation errors', () => {
+    cy.intercept('POST', '/newsletter*', {
+      message: 'Email exists already.',
+    }).as('subscribe'); // for intercepting http requests
+    cy.get('[data-cy="newsletter-email"]').type('test@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@subscribe');
+    cy.contains('Email exists already.');
+  });
 });
