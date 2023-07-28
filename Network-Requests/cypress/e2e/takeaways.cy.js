9b9935925968b41fd7b8246e1a6fbe5fa8a74a2a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-28 14:55:53 -0700

    seeding the test db

--- Network-Requests/cypress/e2e/takeaways.cy.js
+++ Network-Requests/cypress/e2e/takeaways.cy.js
@@ -1,7 +1,11 @@
 /// <reference types="Cypress" />
 
 describe('Takeaways', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.task('seedDatabase');
+  });
   it('should display a list of fetched takeaways', () => {
-    cy.visit('/')
+    cy.get('[data-cy="takeaway-item"]').should('have.length', 2);
   });
 });
