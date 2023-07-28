0743c0b20552ffda25b27f7330a628687438697a
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-28 15:12:27 -0700

    interceptors

--- Network-Requests/cypress/e2e/newsletter.cy.js
+++ Network-Requests/cypress/e2e/newsletter.cy.js
@@ -6,6 +6,7 @@ describe('Newsletter', () => {
     cy.task('seedDatabase');
   });
   it('should display a success messasge', () => {
+    cy.intercept('POST', '/newsletter*', { status: 201 }); // for intercepting http requests
     cy.get('[data-cy="newsletter-email"]').type('test@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
     cy.contains('Thanks for signing up');
