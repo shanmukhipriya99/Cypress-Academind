cc8149cf2a21f14091633cc1576a4e5f743441a0
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2023-07-27 18:10:03 -0700

    fake stub implementation

--- Stubs-Spies-Fixtures/cypress/e2e/location.cy.js
+++ Stubs-Spies-Fixtures/cypress/e2e/location.cy.js
@@ -3,11 +3,22 @@
 describe('share location', () => {
   it('should fetch the user location', () => {
     cy.visit('/').then((windw) => {
-      cy.stub(windw.navigator.geolocation, 'getCurrentPosition').as(
-        'getUserLocation'
-      );
+      cy.stub(windw.navigator.geolocation, 'getCurrentPosition')
+        .as('getUserLocation')
+        .callsFake((cb) => {
+          setTimeout(() => {
+            cb({
+              coords: {
+                latitude: 37.5,
+                longitude: 83.43,
+              },
+            });
+          }, 100);
+        });
     });
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getUserLocation').should('have.been.called');
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+    cy.get('[data-cy="actions"]').should('contain', 'Location fetched'); // alternative to contains()
   });
 });
